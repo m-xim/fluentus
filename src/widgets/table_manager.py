f6122ec40b8aa0e93e95972c9123d5e275b76c68
m-xim <170838360+m-xim@users.noreply.github.com>
2025-02-07 17:10:08 +0300

    fix(theme): correct system operations display in light theme

--- src/widgets/table_manager.py
+++ src/widgets/table_manager.py
@@ -2,12 +2,14 @@ import re
 from collections import defaultdict
 from typing import Optional, Callable, Iterable, Tuple, Dict
 
-from PyQt6.QtGui import QColor, QIcon
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QHeaderView, QTreeWidget, QTreeWidgetItem
 
 from src.fluent_api.FluentAPI import FluentAPI
 from src.fluent_api.base_type.translations import LanguagesType
-from src.utils.config_reader import get_config, TableColumn
+from src.utils.config_reader import get_config, TableColumn, Colors
+from src.utils.icon_utils import get_tinted_icon
 from src.utils.resource_path import resource_path
 
 
@@ -17,10 +19,6 @@ class TableManager:
     # Constants for column indices
     ICON_COLUMN_INDEX = 0
     BASE_COLUMN_COUNT = 2
-
-    # Colors for text highlighting
-    HIGHLIGHT_COLOR = QColor(230, 200, 49)
-    DEFAULT_COLOR = QColor(255, 255, 255)
 
     # Path to the comment icon
     COMMENT_ICON_PATH = resource_path("resource/icons/comment.png")
@@ -32,7 +30,7 @@ class TableManager:
         self,
         table: QTreeWidget,
         fluent_api: FluentAPI,
-        on_item_selected_callback: Optional[Callable[[], None]] = None
+        on_item_selected_callback: Optional[Callable[[], None]] = None,
     ):
         """
         Initializes the TableManager.
@@ -46,12 +44,15 @@ class TableManager:
         self.fluent_api = fluent_api
         self.on_item_selected_callback = on_item_selected_callback
 
-        self.config: TableColumn = get_config(TableColumn, root_key='table_column')
+        self.table_config: TableColumn = get_config(
+            TableColumn, root_key="table_column"
+        )
+        self.colors_config: Colors = get_config(Colors, root_key="colors")
 
         # Header labels
-        self.BASE_HEADERS = [self.config.icon, self.config.variable]
+        self.BASE_HEADERS = [self.table_config.icon, self.table_config.variable]
 
-        self.comment_icon = QIcon(self.COMMENT_ICON_PATH)
+        self.comment_icon = get_tinted_icon(self.COMMENT_ICON_PATH, self.table)
         self._setup_table()
 
     def _setup_table(self) -> None:
@@ -62,8 +63,12 @@ class TableManager:
         if self.on_item_selected_callback:
             self.table.itemSelectionChanged.connect(self.on_item_selected_callback)
 
-        self.table.setColumnCount(self.BASE_COLUMN_COUNT + len(self.fluent_api.get_languages()))
-        self.table.header().setSectionResizeMode(self.ICON_COLUMN_INDEX, QHeaderView.ResizeMode.Fixed)
+        self.table.setColumnCount(
+            self.BASE_COLUMN_COUNT + len(self.fluent_api.get_languages())
+        )
+        self.table.header().setSectionResizeMode(
+            self.ICON_COLUMN_INDEX, QHeaderView.ResizeMode.Fixed
+        )
         self.table.setColumnWidth(self.ICON_COLUMN_INDEX, self.ICON_COLUMN_WIDTH)
 
     def _find_header_index(self, header_name: str) -> Optional[int]:
@@ -88,15 +93,23 @@ class TableManager:
             return
 
         translations = self.fluent_api.translations[variable_name]
-        header_name = f'{self.config.translation} — {language_code}'
+        header_name = f"{self.table_config.translation} — {language_code}"
         column_index = self._find_header_index(header_name)
         if column_index is None:
             return  # Header not found
 
         if attribute_name:
-            self._update_attribute_translation(attribute_item, translations, attribute_name, language_code, column_index)
+            self._update_attribute_translation(
+                attribute_item,
+                translations,
+                attribute_name,
+                language_code,
+                column_index,
+            )
         else:
-            self._update_variable_translation(variable_item, translations, language_code, column_index)
+            self._update_variable_translation(
+                variable_item, translations, language_code, column_index
+            )
 
         self._apply_icon_and_formatting(variable_item, translations, language_code)
 
@@ -105,7 +118,7 @@ class TableManager:
         variable_item: QTreeWidgetItem,
         translations: LanguagesType,
         language_code: str,
-        column_index: int
+        column_index: int,
     ) -> None:
         """Updates the variable item's translation."""
         new_translation = self._extract_text(translations[language_code].value)
@@ -119,10 +132,12 @@ class TableManager:
         translations: LanguagesType,
         attribute_name: str,
         language_code: str,
-        column_index: int
+        column_index: int,
     ) -> None:
         """Updates the attribute item's translation."""
-        new_translation = self._extract_text(translations[language_code].attributes[attribute_name])
+        new_translation = self._extract_text(
+            translations[language_code].attributes[attribute_name]
+        )
         old_translation = attribute_item.text(column_index)
         if new_translation != old_translation:
             attribute_item.setText(column_index, new_translation)
@@ -131,21 +146,26 @@ class TableManager:
         self,
         variable_item: QTreeWidgetItem,
         translations: LanguagesType,
-        language_code: str
+        language_code: str,
     ) -> None:
         """Sets the icon and text formatting based on comments and checks."""
         languages = self.fluent_api.get_languages()
 
         # Set icon
         comment_exists = any(translations[lang].comment for lang in languages)
-        variable_item.setIcon(self.ICON_COLUMN_INDEX, self.comment_icon if comment_exists else QIcon())
+        variable_item.setIcon(
+            self.ICON_COLUMN_INDEX, self.comment_icon if comment_exists else QIcon()
+        )
 
         # Set foreground color
         language_data = translations[language_code]
-        highlight_color = self.HIGHLIGHT_COLOR if language_data.check else self.DEFAULT_COLOR
-        column_index = self._find_header_index(f'{self.config.translation} — {language_code}')
-        if column_index is not None:
-            variable_item.setForeground(column_index, highlight_color)
+        column_index = self._find_header_index(
+            f"{self.table_config.translation} — {language_code}"
+        )
+        if language_data.check:
+            variable_item.setForeground(column_index, self.colors_config.highlight)
+        else:
+            variable_item.setData(column_index, Qt.ItemDataRole.ForegroundRole, None)
 
     def populate_table(self) -> None:
         """
@@ -159,16 +179,15 @@ class TableManager:
         self._initialize_headers(languages)
 
         for variable_name, translations in self.fluent_api.translations.items():
-            table_item = self._create_top_level_item(variable_name, translations, languages)
+            table_item = self._create_top_level_item(
+                variable_name, translations, languages
+            )
             self.table.addTopLevelItem(table_item)
 
         self._restore_selection(selected_variable, selected_attribute)
 
     def _create_top_level_item(
-        self,
-        variable_name: str,
-        translations: LanguagesType,
-        languages: Iterable[str]
+        self, variable_name: str, translations: LanguagesType, languages: Iterable[str]
     ) -> QTreeWidgetItem:
         """
         Creates a top-level table item with translations and attributes.
@@ -189,20 +208,24 @@ class TableManager:
                 comment_exists = True
 
         table_item = QTreeWidgetItem(item_data)
-        table_item.setIcon(self.ICON_COLUMN_INDEX, self.comment_icon if comment_exists else QIcon())
+        table_item.setIcon(
+            self.ICON_COLUMN_INDEX, self.comment_icon if comment_exists else QIcon()
+        )
 
         for language_code in languages:
-            header_name = f'{self.config.translation} — {language_code}'
+            header_name = f"{self.table_config.translation} — {language_code}"
             column_index = self._find_header_index(header_name)
             if column_index is None:
                 continue
             language_data = translations[language_code]
-            highlight_color = self.HIGHLIGHT_COLOR if language_data.check else self.DEFAULT_COLOR
-            table_item.setForeground(column_index, highlight_color)
+            if language_data.check:
+                table_item.setForeground(column_index, self.colors_config.highlight)
+            else:
+                table_item.setData(column_index, Qt.ItemDataRole.ForegroundRole, None)
 
         attributes = self._collect_attributes(translations, languages)
         for attribute_name, attribute_translations in attributes.items():
-            attribute_data = ['', attribute_name] + [
+            attribute_data = ["", attribute_name] + [
                 self._extract_text(attribute_translations[lang]) for lang in languages
             ]
             child_item = QTreeWidgetItem(attribute_data)
@@ -219,11 +242,13 @@ class TableManager:
         :return: The processed string.
         """
         if not content:
-            return ''
-        return re.sub(pattern=FluentAPI.RE_LINE_SPLIT_PATTERN, repl='➚', string=content)
+            return ""
+        return re.sub(pattern=FluentAPI.RE_LINE_SPLIT_PATTERN, repl="➚", string=content)
 
     @staticmethod
-    def _collect_attributes(translations: LanguagesType, languages: Iterable[str]) -> Dict[str, Dict[str, str]]:
+    def _collect_attributes(
+        translations: LanguagesType, languages: Iterable[str]
+    ) -> Dict[str, Dict[str, str]]:
         """
         Aggregates attributes for languages.
 
@@ -236,11 +261,13 @@ class TableManager:
         for language_code in languages:
             language_data = translations[language_code]
             for attr_name, attr_value in language_data.attributes.items():
-                attributes[attr_name][language_code] = attr_value or ''
+                attributes[attr_name][language_code] = attr_value or ""
 
         return attributes
 
-    def _restore_selection(self, variable_name: Optional[str], attribute_name: Optional[str]) -> None:
+    def _restore_selection(
+        self, variable_name: Optional[str], attribute_name: Optional[str]
+    ) -> None:
         """
         Restores the selection based on remembered variable and attribute.
 
@@ -250,7 +277,7 @@ class TableManager:
         if not variable_name:
             return
 
-        variable_column = self._find_header_index(self.config.variable)
+        variable_column = self._find_header_index(self.table_config.variable)
         if variable_column is None:
             return
 
@@ -272,10 +299,14 @@ class TableManager:
 
         :param languages: Iterable of language codes.
         """
-        headers = self.BASE_HEADERS + [f'{self.config.translation} — {lang}' for lang in languages]
+        headers = self.BASE_HEADERS + [
+            f"{self.table_config.translation} — {lang}" for lang in languages
+        ]
         self.table.setHeaderLabels(headers)
 
-    def get_selected_items(self) -> Tuple[Optional[QTreeWidgetItem], Optional[QTreeWidgetItem]]:
+    def get_selected_items(
+        self,
+    ) -> Tuple[Optional[QTreeWidgetItem], Optional[QTreeWidgetItem]]:
         """
         Retrieves the selected item and its parent.
 
@@ -297,10 +328,14 @@ class TableManager:
         :return: A tuple containing the selected variable and attribute.
         """
         parent_item, selected_item = self.get_selected_items()
-        variable_column = self._find_header_index(self.config.variable)
+        variable_column = self._find_header_index(self.table_config.variable)
         if variable_column is None:
             return None, None
 
         variable_name = parent_item.text(variable_column) if parent_item else None
-        attribute_name = selected_item.text(variable_column) if selected_item and parent_item else None
+        attribute_name = (
+            selected_item.text(variable_column)
+            if selected_item and parent_item
+            else None
+        )
         return variable_name, attribute_name
